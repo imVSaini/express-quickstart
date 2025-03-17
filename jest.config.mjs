d49323b2cd2848ae746c3a26a74399b276d2126f
Vaibhav Saini <vaibhav.dev20@gmail.com>
2025-03-18 03:11:50 +0530

    config: enhance jest configuration with clearMocks

--- jest.config.mjs
+++ jest.config.mjs
@@ -6,16 +6,19 @@
 /** @type {import('jest').Config} */
 export default {
   testEnvironment: "node",
+  clearMocks: true,
   rootDir: "./",
   coverageDirectory: "<rootDir>/coverage",
   collectCoverageFrom: [
     "<rootDir>/src/**/*.js",
     "!<rootDir>/src/**/constant.js",
+    "!<rootDir>/src/**/index.js",
+    "!<rootDir>/src/**/types.js"
   ],
   testPathIgnorePatterns: ["<rootDir>/node_modules"],
   coverageReporters: ["json", "html"],
   testMatch: [
     "<rootDir>/__tests__/**/*.test.js",
     "<rootDir>/?(*.)+(spec|test).js"
-  ],
+  ]
 }
